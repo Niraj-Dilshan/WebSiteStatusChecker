de307d354c40bfee828a18f3379cadd9d3cc5a73
INFINITY <nirajnimantha@gmail.com>
2022-11-27 17:14:55 +0530

    finaziling

--- backend/webcheck.py
+++ backend/webcheck.py
@@ -1,8 +1,17 @@
 import requests
-from urllib.request import urlopen
-from urllib.error import URLError
-from urllib.error import HTTPError
 from http import HTTPStatus
+
+import motor.motor_asyncio
+from model import Status
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DB_URL = os.getenv("DB_URL")
+
+client = motor.motor_asyncio.AsyncIOMotorClient(DB_URL)
+
+database = client.StatusList
+collection = database.status
  
 # get the status of a website
 def get_website_status(url):
@@ -18,24 +27,26 @@ def get_status(code):
     return 'Down'
  
 # check status of a list of websites
-def check_status_urls(urls):
+def check_status_urls(urls,name):
     for url in urls:
+        # get the Name of the website
+        i=urls.index(url)
+        sname = name[i]
         # get the status for the website
         code = get_website_status(url)
         # interpret the status
         status = get_status(code)
         # report status
-        print(f'{url:20s}\t{status:5s}\t{code}')
+        collection.update_one({"name": sname}, {"$set": {"status": status}})
 
 #get websites from api
 def get_websites():
     # get the websites from the API
     response = requests.get('http://localhost:8000/api/status')
-    # extract the urls
+    # extract the names
+    name = [website['name'] for website in response.json()]
+    #extract the urls
     url = [website['url'] for website in response.json()]
-    print(url)
-    return url
+    return url , name
 
-URL =  get_websites()
-
-check_status_urls(URL)
+check_status_urls(get_websites()[0], get_websites()[1])
